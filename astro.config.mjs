3fc8016100e868d711e881c244bd48982166d506
Alexavier Brink <65963015+Tubbster-UX@users.noreply.github.com>
2024-10-27 11:20:29 +1100

    added Graphql Support along side a dynamic AO page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
 import react from '@astrojs/react';
 
@@ -7,10 +8,15 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
     }),
   ]
+
 });
