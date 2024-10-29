6f8d861748225a34f31945ca4efa73cef96bae0e
Alexavier Brink <65963015+Tubbster-UX@users.noreply.github.com>
2024-10-29 15:32:35 -0700

    Refactor Card component and mobile navigation
    - Removed unused Card Component
    - Update mobileNavigation component to use TypeScript and handle nested menu items
    - Create backblasts page with dynamic content

--- src/components/mobileNavigation.tsx
+++ src/components/mobileNavigation.tsx
@@ -1,30 +1,41 @@
 import { MenuIcon } from "lucide-react";
 import { useState } from "react";
 
-// todo: set default props
+type MenuItem = {
+    node: {
+        path: string;
+        label: string;
+    };
+};
 
-export default function MobileNavigation(props) {
+type MobileNavigationProps = {
+    menuItems?: MenuItem[];
+};
+
+export default function MobileNavigation({ menuItems = [] }: MobileNavigationProps) {
     const [showDrawer, setShowDrawer] = useState(false);
 
-    return <>
-        <button onClick={() => setShowDrawer(!showDrawer)}>
-            <MenuIcon className="w-10 h-10" size={24} />
-        </button>
-        <aside className={`fixed top-0 transition-all duration-300 ${showDrawer ? "left-0" : "left-full"}`}>
-            <div className="grid grid-cols-2 h-screen w-screen">
-                <div onClick={() => setShowDrawer(false)}>
-                    {/* blank area that triggers close */}
-                </div>
-                {/* menu */}
-                <div className="flex flex-col gap-4 border-l-2 border-secondary bg-secondary text-primary p-12">
-                    {props.menuItems.map((edge) => {
-                        const menuItem = edge.node;
+    return (
+        <>
+            <button onClick={() => setShowDrawer(!showDrawer)}>
+                <MenuIcon className="w-10 h-10" size={24} />
+            </button>
+            <aside className={`fixed top-0 transition-all duration-300 ${showDrawer ? "left-0" : "left-full"}`}>
+                <div className="grid grid-cols-2 h-screen w-screen">
+                    <div onClick={() => setShowDrawer(false)}>
+                        {/* blank area that triggers close */}
+                    </div>
+                    {/* menu */}
+                    <div className="flex flex-col gap-4 border-l-2 border-secondary bg-secondary text-primary p-12">
+                        {menuItems.map((edge) => {
+                            const menuItem = edge.node;
 
-                        // todo: handle nested menu items
-                        return <a href={menuItem.path}>{menuItem.label}</a>
-                    })}
+                            // todo: handle nested menu items
+                            return <a key={menuItem.path} href={menuItem.path}>{menuItem.label}</a>
+                        })}
+                    </div>
                 </div>
-            </div>
-        </aside>
-    </>
+            </aside>
+        </>
+    );
 }
